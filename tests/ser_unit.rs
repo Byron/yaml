728aa814c0441360a9db56024335cc4f244183a4
Sebastian Thiel <byronimo@gmail.com>
2015-06-01 16:32:03 +0200

    tests(ser): mapping flow test impl
    It's derived from block-flow, and will need quite some work until
    it has a chance to succeed.
    
    [skip ci]

--- tests/ser_unit.rs
+++ tests/ser_unit.rs
@@ -146,5 +146,34 @@ fn sequence_flow() {
 
 #[test]
 fn mapping_flow() {
-    panic!("TODO: this test")
+    use structs::DualOptKey;
+    let mut opts = PresentationDetails::yaml();
+    opts.mapping_details.details.style = StructureStyle::Flow;
+    let v = DualOptKey { key1: None, key2: None };
+
+    // null is hidden by default
+    assert_eq!(yaml::to_string_with_options(&v, &opts).unwrap(), "{ key1:, key2: }");
+
+
+    opts.document_indicator_style = Some(DocumentIndicatorStyle::Start(None));
+    assert_eq!(yaml::to_string_with_options(&v, &opts).unwrap(), "--- { key1:, key2: }");
+
+
+    opts.document_indicator_style = Some(DocumentIndicatorStyle::StartEnd(None));
+    assert_eq!(yaml::to_string_with_options(&v, &opts).unwrap(), "--- { key1:, key2: }\n...");
+
+
+    opts.mapping_details.null_style = NullScalarStyle::Show;
+    assert_eq!(yaml::to_string_with_options(&v, &opts).unwrap(), 
+               "--- { key1: null, key2: null }\n...");
+
+    // hiding all null values means we end up with an empty dict, which needs 
+    // to be presented in flow mode
+    opts.mapping_details.null_style = NullScalarStyle::HideEntry;
+    assert_eq!(yaml::to_string_with_options(&v, &opts).unwrap(),
+               "--- {}\n...");
+
+    let v = DualOptKey { key1: None, key2: Some(42) };
+    assert_eq!(yaml::to_string_with_options(&v, &opts).unwrap(),
+               "--- { key2: 42 }\n...");
 }
